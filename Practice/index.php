90d5d479381323c214fac8e8dca240bd0f54e50d
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-09-09 08:57:55 -0700

    Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

--- Practice/index.php
+++ Practice/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>?Untitled Document</title>
+<title>Untitled Document</title>
 </head>
 <body>
 
@@ -12,6 +12,32 @@
 		Block
 		Comment
 	*/
+	//Primitive Data Types!!!
+	$ix = (int)(9/5); //Integer
+	$fx = (float)(9/5); //Float
+	$fy = 3.12e1; //Scientific Notation Float
+	$fz = 3120e-2; //Other Format
+	$hx = 0xff; //Hexadecimal
+	$ox = 077; //Octal
+	$bx = 00010111001; //Binary
+	$xx = True; //Boolean
+	$yx = False; //Boolean
+	$sx = "Fuck this guy.";
+	echo "<p>$ix</p>";
+	echo "<p>$fx</p>";
+	echo "<p>$fy</p>";
+	echo "<p>$fz</p>";
+	echo "<p>$hx</p>";
+	echo "<p>$ox</p>";
+	echo "<p>$bx</p>";
+	echo "<p>$xx, $yx</p>";
+	echo "<p>$sx</p>";
+?>
+
+<?php
+	//Operator Types
+	//Operator precedence = PEMDAS MOTHAFUCKAAA
+	
 ?>
 
 </body>
