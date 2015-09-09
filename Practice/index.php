2fd44cade2f80afb7ab44f7c564e42c7bc63fab9
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-09-09 08:33:45 -0700

    Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

---
+++ Practice/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>?Untitled Document</title>
+</head>
+<body>
+
+<?php
+	//Line Comment
+	/*
+		Block
+		Comment
+	*/
+?>
+
+</body>
+
+
+</html>
