79f2bf19924433c317c0d224b26896763fff649a
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-09-14 08:43:29 -0700

    Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

--- Practice/Homework.php
+++ Practice/Homework.php
@@ -6,14 +6,16 @@
 	<?php
 		/*
 			Brandon Ramirez
+			RCC PHP Class
 			Version 1.2
-			Create a line of code using ternary operators that takes a number 0-100 and returns a string value with a letter grade.
+			Purpose: ternary Operator Examples
 		*/
 	?>
 </head>
 <body>
 
 <?php
+	//Create a line of code using nested ternary operators that takes a number 0-100 and returns a string value with a letter grade.
 	$score = 100;
 	$grade = $score < 60 ? "an F" : (
 	         $score < 70 ? "a D"  : (
