c8908873947cf78e70a885dd14be5b431e9529ff
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-09-16 01:04:21 -0700

    V 1.0
    Added V 1.0 files for homework.

---
+++ 09-16-2015/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Overtime</title>
+		<!--
+			
+			Brandon Ramirez
+			CIS12 - PHP Programming
+			09/15/2015 Version 1.0
+			Purpose: Demonstrate knowledge of conditional statements in PHP
+			
+		-->
+	</head>
+	<body>
+		<form action="push_form.php" method="post">
+			<p>
+				How many hours did you work?
+			</p>
+			<p>
+				<input type="text" name="hours" /> 
+				<!-- Takes form input and send to a PHP page -->
+			</p>
+		</form>
+	</body>
+</html>
---
+++ 09-16-2015/push_form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Paycheck</title>
+		<!--
+			
+			Brandon Ramirez
+			CIS12 - PHP Programming
+			09-15-2015 Version 1.0
+			Purpose: To demonstrate understanding of conditional statements in PHP
+			
+		-->
+	</head>
+	<body
+		
+		<p>
+			<?php
+				$wage = 15; //Given in $/hr
+				$x 	  = $_POST["hours"];
+				if 		(!is_numeric($x)) $str = "You did not enter a number!"; //Checks if input is a number
+				else if ($x < 0) 		  $str = "You did not enter a valid number!"; //Checks if input is positive integer
+				else {
+					$amt = ($x <= 20) ? $x * $wage 						   : (
+					       ($x <= 40) ? ($x - 20) * $wage * 2 + 20 * $wage : (
+						   				($x - 40) * $wage * 3 + 20 * $wage * 2 + 20 * $wage));
+					$str = "You made $".$amt." this week! Go you!";
+				}
+				echo $str;
+			?>
+		</p>
+		
+	</body>
+</html>
