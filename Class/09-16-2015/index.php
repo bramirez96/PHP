8c51ab67e875820b42724a563d57949e1203bac6
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-09-16 08:18:42 -0700

    Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

---
+++ Class/09-16-2015/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Looping... I Think?</title>
+		<?php
+			/*
+				Brandon Ramirez
+				CIS12 - PHP Programming
+				09/16/2015 Version 1.0
+				Purpose: Unsure
+			*/
+		?>
+	</head>
+	<body>
+	
+		<?php
+			/*
+				For x = T and y = F
+				x^y = T
+				x^y^x = F
+				x^y^y = T
+				
+				DeMorgan's Law:
+					!(x && y) = !x || !y
+					!(x || y) = !x && !y
+			*/
+		?>
+	
+	</body>
+</html>
