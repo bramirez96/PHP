3bb570e6886d4588e8edbbfb3fb2c9bfe3109e38
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-09-21 08:12:54 -0700

    v1.0 Created Page Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

---
+++ Class/09-21-2015/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Truth Table</title>
+			<?php
+				/*
+					Brandon Ramirez
+					CIS12 - PHP Programming
+					09/21/2015 v1.0
+					Purpose: IDK man
+				*/
+			?>
+	</head>
+	<body>
+	
+	
+	
+	
+	</body>
+</html>
