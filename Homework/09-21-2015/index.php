90f1c3b8cd6d487b893d5a77490a03fdb8e0cd0d
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-09-22 09:50:58 -0700

    v1.2 09/22/15 Added more variables for increased code flexibility. Made table head change dynamically with interest variable values Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

--- Homework/09-21-2015/index.php
+++ Homework/09-21-2015/index.php
@@ -7,7 +7,7 @@
 			/*
 				Brandon Ramirez
 				CIS12 - PHP Programming
-				09/21/2015 v1.0
+				09/21/2015 v1.2
 				Purpose: To create a table that returns amount after interest
 			*/
 		?>
@@ -36,47 +36,46 @@
 				//Declare Variables
 				$prin   = 100; //Principal value
 				$nYears = 50;  //Number of years accruing interest
-				$x5		= $prin; 
-				$x6		= $prin; //Initializing principals for each column
-				$x7		= $prin; //based off of the original principal
-				$x8		= $prin; //value for easy changing and debugging.
-				$x9 	= $prin;
-				$x0		= $prin;
-				//Not putting interest values into a variable, 
-				//since they are decalred in table head as set
+				$x1	= $x2 = $x3	= $x4 = $x5 = $x6 = $prin; //Initialize variables for each column to $prin value
+				$i1 = .05; //Interest values for each column
+				$i2 = .06;
+				$i3 = .07;
+				$i4 = .08;
+				$i5 = .09;
+				$i6 = .10;
 				//Create table head
 				echo "<tr>
 						<th>Years</th>
-						<th>At 5%</th>
-						<th>At 6%</th>
-						<th>At 7%</th>
-						<th>At 8%</th>
-						<th>At 9%</th>
-						<th>At 10%</th>
+						<th>At ".($i1 * 100)."%</th>
+						<th>At ".($i2 * 100)."%</th>
+						<th>At ".($i3 * 100)."%</th>
+						<th>At ".($i4 * 100)."%</th>
+						<th>At ".($i5 * 100)."%</th>
+						<th>At ".($i6 * 100)."%</th>
 					  </tr>";
 				for ($year = 1; $year <= $nYears; $year++) {
 					//Accrue Interest!
-					$x5 *= (1 + 0.05);
+					$x1 *= (1 + $i1);
+					number_format($x1, 2, ".", "");
+					$x2 *= (1 + $i2);
+					number_format($x2, 2, ".", "");
+					$x3 *= (1 + $i3);
+					number_format($x3, 2, ".", "");
+					$x4 *= (1 + $i4);
+					number_format($x4, 2, ".", "");
+					$x5 *= (1 + $i5);
 					number_format($x5, 2, ".", "");
-					$x6 *= (1 + 0.06);
+					$x6 *= (1 + $i6);
 					number_format($x6, 2, ".", "");
-					$x7 *= (1 + 0.07);
-					number_format($x7, 2, ".", "");
-					$x8 *= (1 + 0.08);
-					number_format($x8, 2, ".", "");
-					$x9 *= (1 + 0.09);
-					number_format($x9, 2, ".", "");
-					$x0 *= (1 + 0.10);
-					number_format($x0, 2, ".", "");
 					//Output values to table body
 					echo "<tr>
 							<td>$year</td>
+							<td>$".number_format($x1, 2)."</td>
+							<td>$".number_format($x2, 2)."</td>
+							<td>$".number_format($x3, 2)."</td>
+							<td>$".number_format($x4, 2)."</td>
 							<td>$".number_format($x5, 2)."</td>
 							<td>$".number_format($x6, 2)."</td>
-							<td>$".number_format($x7, 2)."</td>
-							<td>$".number_format($x8, 2)."</td>
-							<td>$".number_format($x9, 2)."</td>
-							<td>$".number_format($x0, 2)."</td>
 						  </tr>";
 				}
 			?>
