9483b9fbe8554eba2535f2552b6db93ab7e391ff
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-09-30 09:26:30 -0700

    Basically nothing. It's KIND OF notes, but not really... Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

---
+++ Class/09-30-2015/classes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Classes</title>
+		<?php
+			/*
+				Brandon Ramirez
+				CIS-12 PHP
+				09/30/2015
+				Purpose: Learn how to use classes
+			*/
+			//Look up "catch 22"
+			//Deck of cards:
+			//Deck has array of 52 cards attribute, card back attribute
+			//Also has functions to shuffle and deal
+			//Cards have suit, value, picture attribute
+			//You could probably do your social network class now...
+			//User class with some gr8 methods
+		?>
+	</head>
+	<body>
+	
+	</body>
+</html>
