2837beea02c1e623ab9ee57077cb9410411f408a
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-10-05 09:26:03 -0700

    Basically nothing... I hate this class. Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

---
+++ Class/databases.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Databases</title>
+		<?php
+			/**
+			* Brandon Ramirez
+			* CIS12 - PHP Programming
+			* 10/05/2015
+			* Purpose: To learn MySQL
+			*/
+		?>
+	</head>
+	<body>
+		<?php
+			//Every table starts with a primary key
+			//This guy's a Dr. Doofenschmirtz......
+			//Split into book, subject, author, and publisher tables
+			//For book, use ISBN
+			//For subject, use dewey decimal system
+			//For author, just enumerate it?
+			//For publisher, enumerate it
+		?>
+	</body>
+</html>
