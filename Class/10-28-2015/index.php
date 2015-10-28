995415416872f9b8da2bc78aa716f4f1fdcaf643
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-10-28 09:29:21 -0700

    Just ome /notes/ for class Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

---
+++ Class/10-28-2015/index.php
@@ -0,0 +1,36 @@
+<?php //index.php
+	/**
+	* Brandon Ramirez
+	* 10/28/2015
+	* Purpose: To do something in class
+	*/
+$page_title = "All I do is take notes...";
+
+<<<_END
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>$page_title</title>
+	</head>
+	<body>
+	
+	</body>
+</html>
+_END
+/*
+Surveys should have a survey id, posted in GET object in the url hellz yeah
+Try to Include... COMMENTS (text type), CHECK BOXES MULTIPLE ANSWERS (not radio buttons)
+
+
+LEARN HOW TO PAGINATE DATA FROM SQL QUERIES
+MAKE SURE YOU DON'T ALLOW DUPLICATE ITEMS IN YOUR TABLE
+INDEX YOUR DATABASE, LOOK UP WHAT THAT MEANS (lol)
+NEVER FORGET TO CLOSE YOUR DATABASE AT THE END OF FILE
+# OF RECORDS / RESULTS PER PAGE,  <<ROUND UP>> IF RECORDS > DISPLAY
+$var = isset($_GET['something']) ? $_GET['something'] : 'default value';
+INCLUDE ABILITY TO SORT ASCENDING AND DESCENDING
+WRAP TABLE HEADERS IN SORT QUERY LINKS THAT REFERENCE THE SAME PAGE
+<a href="thispage.php?sort=name">Name</a>
+*/
+?>
