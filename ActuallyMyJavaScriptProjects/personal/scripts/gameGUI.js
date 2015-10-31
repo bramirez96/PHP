217b738ccc2550b8f8ff57ac8e856386c3b50a7c
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-10-30 17:23:17 -0700

    Remade index and reworked login/signup to be more effective

---
+++ ActuallyMyJavaScriptProjects/personal/scripts/gameGUI.js
@@ -0,0 +1,14 @@
+/**
+* Header GUI Piece
+*/
+function makeHeader(header) {
+	header.innerHTML = ' \
+		<nav>\
+			<ul>\
+				<li><a href="./home.html" class="menuItem" title="Home">Home</a></li>\
+				<li><a href="./map.html" class="menuItem" title="Map">Map</a></li>\
+				<li><a href="./hero.html" class="menuItem" title="Hero">Hero</a></li>\
+				<li><a href="./inventory.html" class="menuItem" title="Inventory">Inventory</a></li>\
+			</ul>\
+		</nav>'
+}
