6fb3b7ea0baa7fc7ea97aae7abf20dbc2753e873
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-11-02 13:09:52 -0800

    more changes to the same thing. maybe now I can actually get somewhere? Next: 	RegExp to clean and validate input 	DO OTHER ASSIGNMENT YOU FUCKWAD Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

--- ActuallyMyJavaScriptProjects/personal/scripts/gameGUI.js
+++ ActuallyMyJavaScriptProjects/personal/scripts/gameGUI.js
@@ -4,32 +4,9 @@
 * makeGUI() calls all GUI functions
 */
 function makeGUI() {
-	makeHeader();
-	makeUserModule();
+	$("#headModule").load("./modules/headModule.html");
+	$("#userModule").load("./modules/userModule.html");
 }
-/**
-* makeHeader() populates #headModule with menu items
-*/
-function makeHeader() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("headModule").innerHTML = xhttp.responseText;
-    }
-  }
-  xhttp.open("GET", "./modules/headModule.html", true);
-  xhttp.send();
-}
-/**
-* makeUserModule() populates #userModule with user information
-*/
-function makeUserModule() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("userModule").innerHTML = xhttp.responseText;
-    }
-  }
-  xhttp.open("GET", "./modules/userModule.html", true);
-  xhttp.send();
-}
+$(document).ready(function() {
+	makeGUI();
+});
--- ActuallyMyJavaScriptProjects/personal/scripts/main.js
+++ ActuallyMyJavaScriptProjects/personal/scripts/main.js
@@ -1,5 +1,20 @@
 //main.js main functions for thingamajig
-
+var queryString = {}; //set it as global for use bye functions
+function stripURL() {
+	var myExp = /([^?&]+)=([^=&]*)/g;
+	var url = location.href;
+	var x;
+	do {
+		x = myExp.exec(url);
+		if (x) {
+			queryString[x[1]] = x[2];
+		}
+	} while (x);
+	/*location.href.replace(
+		new RegExp("([^?&]+)=([^=&]*)", "g"),
+		function(x, y, z) { queryString[y] = z; }
+	);*/
+};
 /**
 * loadUsers() loads stored JSON strings and parses them into objects
 * Eventually move this to a game-specific file
@@ -9,7 +24,6 @@ function loadUsers() {
 	else z = '{"keys":[]}';
 	usr = JSON.parse(z);
 }
-document.addEventListener(onload, loadUsers());
 function logOut() {
 	sessionStorage.clear();
 	alert("You will now be returned to the login screen.");
@@ -22,22 +36,23 @@ function logOut() {
 function checkLoginForm(form) {
 	x = form.elements["user"].value;
 	y = form.elements["pass"].value;
-	document.getElementById("passError").style.display="none";
-	document.getElementById("userError").style.display="none";
+	$("#passError").css("display", "none");
+	$("#userError").css("display", "none");
 	if (usr[x]) {//If storage item exists
 		//alert("username exists");
 		if ((usr[x]["pass"]) == y){//Check the password
 			//alert("and your password is right");
+			form.elements["key"].value = usr[x]["key"];
 			return true;
 		} else {
-			document.getElementById("passError").style.display="inline";
+			$("#passError").css("display", "inline");
 			//alert("but your password is wrong");
 			return false;
 		}
 	} else {
 		//alert("username doesnt exist");
-		document.getElementById("userError").style.display="inline";
-		document.getElementById("passError").style.display="inline";
+		$("#userError").css("display", "inline");
+		$("#passError").css("display", "inline");
 		return false;
 	}
 }
@@ -49,28 +64,28 @@ function checkSignupForm(form) {
 	x = form.elements["user"].value;
 	y = form.elements["pass"].value;
 	z = form.elements["pVer"].value;
-	document.getElementById("userError").style.display="none";
-	document.getElementById("passError").style.display="none";
-	document.getElementById("nullError").style.display="none";
+	$("#userError").css("display", "none");
+	$("#passError").css("display", "none");
+	$("#nullError").css("display", "none");
 	if (usr[x] == null) { //If username isn't taken
 		if (y != "") {
 			if (y == z) {
 				return true;
 			} else {
-				document.getElementById("passError").style.display="inline";
+				$("#passError").css("display", "inline");
 				return false;
 			}
 		} else {
-			document.getElementById("nullError").style.display="inline";
+			$("#nullError").css("display", "inline");
 			return false;
 		}
 	} else {
-		document.getElementById("userError").style.display="inline";
+		$("#userError").css("display", "inline");
 		if (y == "") {
-			document.getElementById("nullError").style.display="inline";
+			$("#nullError").css("display", "inline");
 		}
 		if (y != z) {
-			document.getElementById("passError").style.display="inline";
+			$("#passError").css("display", "inline");
 		}
 		return false;
 	}
