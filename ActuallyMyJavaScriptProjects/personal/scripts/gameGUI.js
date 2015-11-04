a1fdfa49fa58985a88b5c2a33873d46cd294b083
ToEndThePeace <bran.ramirez.don@gmail.com>
2015-11-04 00:22:57 -0800

    Got some great shit done. Basically, character is finished, and experience can be added. levels can be #gainz. Um, that’s about it. I think. I did a lot, I promise…

--- ActuallyMyJavaScriptProjects/personal/scripts/gameGUI.js
+++ ActuallyMyJavaScriptProjects/personal/scripts/gameGUI.js
@@ -1,8 +1,8 @@
 //CHANGING THE FILE TO CONTROL ALL GAME FUNCTIONS DEAL WITH IT
 
 function loadChars() {
-	if (localStorage.char) var z = localStorage.char;
-	else z = '[]';
+	if (localStorage.char) window.z = localStorage.char;
+	else window.z = '[]';
 	char = JSON.parse(z);
 }
 /**
@@ -14,40 +14,53 @@ function makeGUI() {
 }
 $(document).ready(function() {
 	loadChars();
-	key = sessionStorage.num;
+	window.key = sessionStorage.num;
 	makeGUI();
 });
 $(window).unload(function() { //Saves character to local variable always, so goood
-	x = JSON.stringify(char);
+	if (typeof curChar !== "undefined") char[key] = curChar;
+	var x = JSON.stringify(char);
 	localStorage.char = x;
 });
-function makeHeader() {
+function makeHeader() { //Calls ajax function that loads header module
 	$("#headModule").load("./modules/headModule.html");
 }
 function makeUser() {
-	if (char[key]) { //user has a character
-		$("#userModule").load("./modules/userModule.html", function() {
-			setCharacter(char[key]);
-			curChar = char[key];
-		});
-	} else {
-		$("#userModule").load("./modules/newCharacterModule.html");
+	if (char[key]) { //If user has a character YOU NEED TO REINITIALIZE IT
+		loadUserModule(char[key]);
+	} else { //If user needs to make a character
+		makeNewCharacterModule();
 	}
-	
+}
+function loadUserModule(xyz) { //Ajax function that loads user module and sets character info
+	xyz = (typeof xyz !== "undefined") ? xyz : curChar;
+	$("#userModule").load("./modules/userModule.html", function() {//REINITIALLIZE IT HERE
+		var n = xyz;
+		window.curChar = new Character(n.name, n.role, n.bStats, n.stats, n.hp, n.mp, n.xp, n.lvl, n.usrImg);
+		setCharacter(curChar);
+	});
+}
+function makeNewCharacterModule() { //
+	$("#userModule").load("./modules/newCharacterModule.html");
+}
+function makeCharacter(name, role) {
+	z = new Character(name, role);
+	char[key] = z;
 }
 function setCharacter(x) { //Needs to take character instance object as parameter
 	//Use this function to set the stats of the character
 	//Should be pulled from the character object.... if that's a thing
 	$("#usrImg").attr("src", x.usrImg);
 	$("#level").text(x.lvl);
-	var hp    = x.hp[0]; 
-	var maxHp = x.hp[1];
+	$("#name").text(x.name);
+	var hp    = x.hp[0].toFixed(2); 
+	var maxHp = x.hp[1].toFixed(2);
 	$("#hpNow").text(hp);
 	$("#hp").val(hp);
 	$("#hpMax").text(maxHp);
 	$("#hp").attr("max", maxHp);
-	var mp    = x.mp[0]; 
-	var maxMp = x.mp[1];
+	var mp    = x.mp[0].toFixed(0);
+	var maxMp = x.mp[1].toFixed(0);
 	$("#mpNow").text(mp);
 	$("#mp").val(mp);
 	$("#mpMax").text(maxMp);
@@ -58,10 +71,10 @@ function setCharacter(x) { //Needs to take character instance object as paramete
 	$("#xp").val(xp);
 	$("#xpMax").text(maxXp);
 	$("#xp").attr("max", maxXp);
-	$("#atk").text(x.stats.atk);
-	$("#mag").text(x.stats.mag);
-	$("#def").text(x.stats.def);
-	$("#spd").text(x.stats.spd);
+	$("#atk").text(x.stats.atk.toFixed(2));
+	$("#mag").text(x.stats.mag.toFixed(2));
+	$("#def").text(x.stats.def.toFixed(2));
+	$("#spd").text(x.stats.spd.toFixed(2));
 }
 function checkChar(form) {
 	x = form.elements["charName"].value;
@@ -70,7 +83,8 @@ function checkChar(form) {
 	$("#classError").css("display", "none");
 	if (x != "" && x.length > 4) {
 		makeCharacter(x, y);
-		return true; // !!!!!Has to submit eventually, change this!
+		loadUserModule();
+		return false; // !!!!!Has to submit eventually, change this!
 	}
 	else {
 		$("#nameError").css("display", "inline");
@@ -80,11 +94,17 @@ function checkChar(form) {
 /**
 * The following code is all information for the character class
 */
-function Character(name, role) { //Character class
-	this.name = name;
-	this.bStats = {atk: 0, mag: 0, def: 0, spd: 0, hp: 0, mp: 0};
-	this.stats = {atk: 0, mag: 0, def: 0, spd: 0, hp: 0, mp: 0};
-	if (role == "mage") {
+function Character(name, role, base, stats, hp, mp, xp, lvl, img) { //Character class
+	this.name   = typeof  name !== "undefined" ?  name : "Guest";
+	this.role   = typeof  role !== "undefined" ?  role : "mage";
+	this.bStats = typeof  base !== "undefined" ?  base : {atk: 0, mag: 0, def: 0, spd: 0, hp: 0, mp: 0};
+	this.stats  = typeof stats !== "undefined" ? stats : {atk: 0, mag: 0, def: 0, spd: 0, hp: 0, mp: 0};
+	this.hp     = typeof    hp !== "undefined" ?    hp : [0, 0];
+	this.mp     = typeof    mp !== "undefined" ?    mp : [0, 0];
+	this.xp     = typeof    xp !== "undefined" ?    xp : [0, 8];
+	this.lvl    = typeof   lvl !== "undefined" ?   lvl : 1;
+	this.usrImg = typeof   img !== "undefined" ?   img : "./images/img_frame.png"; //One day: "./images/char_" + this.name + ".png";
+	if (role == "mage" || role == 0) {
 		this.role = 0;
 		this.bStats.atk = 60;
 		this.bStats.mag = 80;
@@ -92,7 +112,7 @@ function Character(name, role) { //Character class
 		this.bStats.spd = 72;
 		this.bStats.hp  = 80;
 		this.bStats.mp  = 80;
-	} else if (role == "warrior") {
+	} else if (role == "warrior" || role == 1) {
 		this.role = 1;
 		this.bStats.atk = 85;
 		this.bStats.mag = 62;
@@ -109,37 +129,28 @@ function Character(name, role) { //Character class
 		this.bStats.hp  = 65;
 		this.bStats.mp  = 75;
 	}
-	this.lvl = 1;
-	this.usrImg = "./images/img_frame.png";//One day: "./images/char_" + this.name + ".png";
-	this.hp = [0, 0];
-	this.mp = [0, 0];
-	this.xp = [0, 8];
-	this.addXP = function(plus) {
-		this.xp[0] = this.xp[0] + plus;
-		if (this.xp[0] >= this.xp[1]) { //What to do if character levels up
-			var cur = this.lvl;
-			var z = Math.cbrt(this.x[0]);
-			this.lvl = Math.floor(z);
-			this.xp[1] = Math.pow(this.lvl + 1, 3);
-			this.setStats();
-			alert("Your character has grown from level " + cur + " to level " + this.lvl + "!");
-		}
-	}
-	this.setStats = function() {
-		this.stats.atk = (this.bStats.atk * 2 * this.lvl / 100) + 5; //This could easily be put
-		this.stats.mag = (this.bStats.mag * 2 * this.lvl / 100) + 5; //into a nice for loop
-		this.stats.def = (this.bStats.def * 2 * this.lvl / 100) + 5;
-		this.stats.spd = (this.bStats.spd * 2 * this.lvl / 100) + 5;
-		this.stats.hp  = (this.bStats.hp  * 2 * this.lvl / 100) + 10 + this.lvl;
-		this.stats.mp  = (this.bStats.mp  * 2 * this.lvl / 100) + 10 + this.lvl;
-		this.hp = [this.stats.hp, this.stats.hp];
-		this.mp = [this.stats.mp, this.stats.mp];
-	}
 	this.setStats();
 }
-function makeCharacter(name, role) {
-	z = new Character(name, role);
-	char[key] = z;
+Character.prototype.addXP = function(plus) {
+	this.xp[0] = this.xp[0] + plus;
+	if (this.xp[0] >= this.xp[1]) { //What to do if character levels up
+		var cur = this.lvl;
+		var z = Math.cbrt(this.xp[0]);
+		this.lvl = Math.floor(z);
+		this.xp[1] = Math.pow(this.lvl + 1, 3);
+		this.setStats();
+		alert("Your character has grown from level " + cur + " to level " + this.lvl + "!");
+	}
+}
+Character.prototype.setStats = function() {
+	this.stats.atk = (this.bStats.atk * 2 * this.lvl / 100) + 5; //This could easily be put
+	this.stats.mag = (this.bStats.mag * 2 * this.lvl / 100) + 5; //into a nice for loop
+	this.stats.def = (this.bStats.def * 2 * this.lvl / 100) + 5;
+	this.stats.spd = (this.bStats.spd * 2 * this.lvl / 100) + 5;
+	this.stats.hp  = (this.bStats.hp  * 2 * this.lvl / 100) + 10 + this.lvl;
+	this.stats.mp  = (this.bStats.mp  * 2 * this.lvl / 100) + 10 + this.lvl;
+	this.hp = [this.stats.hp, this.stats.hp];
+	this.mp = [this.stats.mp, this.stats.mp];
 }
 
 
